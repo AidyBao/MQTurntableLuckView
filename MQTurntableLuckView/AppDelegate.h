4ea8eca7030e7100d0d199a7c8b2a441d0df722a
AidyBao <281907061@qq.com>
2018-04-12 16:25:48 +0800

    Initial Commit

---
+++ MQTurntableLuckView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MQTurntableLuckView
+//
+//  Created by 120v on 2018/4/12.
+//  Copyright © 2018年 MQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
